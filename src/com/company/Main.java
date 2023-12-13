220e831da0195e9e715c05984ea82a64113af245
admin <n.khaimuldin@astanait.edu.kz>
2023-12-13 08:32:37 +0600

    Solution of finding min and max between 3 numbers

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    double a,b,c;
+        Scanner sc = new Scanner(System.in);
+        a=sc.nextDouble();
+        b=sc.nextDouble();
+        c=sc.nextDouble();
+        double min = min(min(a, b), min(b, c));
+        double max = max(max(a,b), max(b,c));
+        System.out.println(min+" "+max);
+    }
+    public static double min(double a, double b){
+            return a<b?a:b;
+    }
+    public static double max(double a, double b){
+        return a>b?a:b;
+    }
+
+}
